37d5d47df4f0d443754f260ac9cede1cd68ec6a6
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-01-23 11:47:57 +0100

    Instance restDB + getAllRecettes

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+const restDB = axios.create({
+    headers:
+        {
+            'cache-control': 'no-cache',
+            'x-apikey': '63ce63d3969f06502871b11b',
+            'content-type': 'application/json'
+        },
+    json: true
+})
 
-app.get('/', function(req, res) {
+app.get('/recettes', async function (req, res) {
 
-    res.send('Hello !')
+
+    var recettes = await restDB.get('https://restdbtest-6339.restdb.io/rest/recettes')
+    res.send(recettes.data)
+    console.log(recettes.data)
+
 })
 
 app.listen(3000, function() {
