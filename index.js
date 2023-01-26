9a777514b4c1e4c3119a7a79b4bc568c35d9c567
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-01-26 17:58:33 +0100

    Procedure autentification

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: secret
 }
+passport.use(
+    new JwtStrategy(jwtOptions, async function(payload, next) {
+        const users = await restDB.get(`https://restdbtest-6339.restdb.io/rest/utilisateurs?q={"name":"${payload.name}"}`)
+        for (let i = 0; i<users.data.length;i++){
+            console.log(users.data[i])
+            if (users.data[i].name === payload.name) {
+                next(null, users.data[i])
+            } else {
+                next(null, false)
+            }
+        }
+
+
+    })
+)
+
+app.use(passport.initialize())
 
 app.get('/recettes', async function (req, res) {
     var recettes = await restDB.get('https://restdbtest-6339.restdb.io/rest/recettes')
@@ -39,7 +56,7 @@ app.post('/user/create',urlEncodedParser, async function (req, res) {
 })
 
 
-app.post('/recettes/create',urlEncodedParser, async function (req, res) {
+app.post('/recettes/create',urlEncodedParser, passport.authenticate('jwt', { session: false }), async function (req, res) {
     
     try{
         var recette = await restDB.put('https://restdbtest-6339.restdb.io/rest/recettes/' + req.body.id, {name: req.body.name})
@@ -53,13 +70,15 @@ app.post('/recettes/create',urlEncodedParser, async function (req, res) {
 
 app.post('/recettes/connexion',urlEncodedParser, async function (req, res) {
     try{
-        const user = await restDB.get(`https://restdbtest-6339.restdb.io/rest/utilisateurs?q={"name":"${req.body.name}","password":"${req.body.password}"}`)
-        if(!user.data.length){
+        const user = await restDB.get(`https://restdbtest-6339.restdb.io/rest/utilisateurs?q={"name":"${req.body.name}"}`)
+        if(!user.data.length ||req.body.password !== user.data[0].password){
             res.sendStatus(401)
             return;
         }
 
-        const userJwt = jwt.sign({ name: user.data.name }, secret)
+
+
+        const userJwt = jwt.sign({ name: user.data[0].name }, secret)
 
         res.json({ jwt: userJwt })
         return;
@@ -71,7 +90,7 @@ app.post('/recettes/connexion',urlEncodedParser, async function (req, res) {
 })
 
 
-app.post('/recettes/create', async function (req, res) {
+/*app.post('/recettes/create', async function (req, res) {
 
     if(req.body.id){
         try{
@@ -94,9 +113,9 @@ app.post('/recettes/create', async function (req, res) {
             return;
         }
     }
-});
+});*/
 
-app.delete('/recettes/delete/:id', async function (req, res) {
+app.delete('/recettes/delete/:id', passport.authenticate('jwt', { session: false }), async function (req, res) {
     try{
         var recette = await restDB.delete('https://restdbtest-6339.restdb.io/rest/recettes/'+req.params.id)
         res.send(recette.statusText)
@@ -127,7 +146,7 @@ app.post('*',async function (req, res) {
     res.sendStatus(404)
 })
 
-app.use(passport.initialize())
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
@@ -137,3 +156,5 @@ function errorCatcheur(error, res){
     console.log(error)
     res.send(error.response.statusText)
 }
+
+
