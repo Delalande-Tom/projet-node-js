23bc200ee8b7d4bf5f1009a776b3831f8becc0f4
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-01-27 10:24:27 +0100

    recette add category,dificulty

--- recettes.js
+++ recettes.js
@@ -13,16 +13,19 @@ async function getAll (req, res) {
  * Create or modify a recette
  */
 async function create (req, res) {
-    if(req.body.name==null || req.body.price == null){
+    if(req.body.name==null || req.body.price == null || req.body.category == null ||req.body.dificulty == null|| req.body.dificulty<1 || req.body.dificulty>5){
         res.sendStatus(400)
+        return;
     }
     try{
-        var recette = await restDB.put('https://restdbtest-6339.restdb.io/rest/recettes/' + req.body.id, {name: req.body.name, price: req.body.price})
+        var recette = await restDB.put('https://restdbtest-6339.restdb.io/rest/recettes/' + req.body.id, {name: req.body.name, price: req.body.price, category: req.body.category, dificulty: req.body.dificulty})
         console.log(recette)
         res.send(recette.statusText)
+        return;
     }catch(error){
-        var recette = await restDB.post('https://restdbtest-6339.restdb.io/rest/recettes', {name: req.body.name, price: req.body.price})
+        var recette = await restDB.post('https://restdbtest-6339.restdb.io/rest/recettes', {name: req.body.name, price: req.body.price, category: req.body.category, dificulty: req.body.dificulty})
         res.send(recette.statusText)
+        return;
     }
 }
 
