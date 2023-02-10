86ea3a77a852477e5f9d6f7205c5786a126430d4
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-02-10 16:40:01 +0100

    Add weight and nbPerson

--- recettes.js
+++ recettes.js
@@ -14,17 +14,17 @@ async function getAll (req, res) {
  * Create or modify a recette
  */
 async function create (req, res) {
-    if(req.body.name==null || req.body.price == null || req.body.category == null ||req.body.difficulty == null|| req.body.difficulty<1 || req.body.difficulty>5 || req.body.description == null || req.body.picture ==null){
+    if(req.body.name==null || req.body.price == null || req.body.category == null ||req.body.difficulty == null|| req.body.difficulty<1 || req.body.difficulty>5 || req.body.description == null || req.body.picture ==null || req.body.nbPerson<=0 || req.body.weight<=0){
         res.sendStatus(400)
         return;
     }
     try{
-        var recette = await restDB.put('https://restdbtest-6339.restdb.io/rest/recettes/' + req.body.id, {name: req.body.name, price: req.body.price, category: req.body.category, difficulty: req.body.difficulty, description:  req.body.description, picture: req.body.picture })
+        var recette = await restDB.put('https://restdbtest-6339.restdb.io/rest/recettes/' + req.body.id, {name: req.body.name, price: req.body.price, category: req.body.category, difficulty: req.body.difficulty, description:  req.body.description, picture: req.body.picture, nbPerson: req.body.nbPerson, weight: req.body.weight})
         console.log(recette)
         res.send(recette.statusText)
         return;
     }catch(error){
-        var recette = await restDB.post('https://restdbtest-6339.restdb.io/rest/recettes', {name: req.body.name, price: req.body.price, category: req.body.category, difficulty: req.body.difficulty, description:  req.body.description, picture: req.body.picture})
+        var recette = await restDB.post('https://restdbtest-6339.restdb.io/rest/recettes', {name: req.body.name, price: req.body.price, category: req.body.category, difficulty: req.body.difficulty, description:  req.body.description, picture: req.body.picture, nbPerson: req.body.nbPerson, weight: req.body.weight})
         res.send(recette.statusText)
         return;
     }
